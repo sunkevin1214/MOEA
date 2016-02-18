5640659662001e4059d48a6b0ed8a8d33ef1690a
sunkevin1214 <yanan.sun@okstate.edu>
2016-02-19 01:49:13 +0800

    optimize the uniform sample file

--- scripts_matlab/uniformly generate samples/test.m
+++ scripts_matlab/uniformly generate samples/test.m
@@ -1,11 +1,12 @@
 clc;clear;
 objs = 3; % set the number of objectives
-divs = 12; % how many divisions you want in each coordinate
-
-global points; % use to save all the sampled points
-points = [];
-p = zeros(1, objs); % temporary vector
-
-uniform_generate(p, objs, divs, divs, 0);
-% scatter3(points(:,1), points(:,2), points(:,3), 'LineWidth', 2);
-% grid;
+divs = 4; % how many divisions you want in each coordinate
+d = uniform_sample(objs, divs);
+% 
+% global points; % use to save all the sampled points
+% points = [];
+% p = zeros(1, objs); % temporary vector
+% 
+% uniform_generate(p, objs, divs, divs, 0);
+% % scatter3(points(:,1), points(:,2), points(:,3), 'LineWidth', 2);
+% % grid;
--- scripts_matlab/uniformly generate samples/uniform_sample.m
+++ scripts_matlab/uniformly generate samples/uniform_sample.m
@@ -1,4 +1,12 @@
-function uniform_generate(p, objs, left, total, element)
+function data = uniform_sample(objs, divs)
+    global points;
+    points = [];
+    p = zeros(1, objs);
+    g(p, objs, divs, divs, 0);
+    data = points;
+end
+
+function g(p, objs, left, total, element)
 %% this function are used to uniformly generate samples in the hyper-plane with intercept in each coordinate with 1.
     global points;
     if element == objs - 1
@@ -7,7 +15,7 @@ function uniform_generate(p, objs, left, total, element)
     else
         for i=0:left
             p(element+1) = i/total;
-            uniform_generate( p, objs, left-i, total, element+1);
+            g( p, objs, left-i, total, element+1);
         end
     end
 end
