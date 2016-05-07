ca79e93fb0c638c21218bc20c2cd543a11faac8d
sunkevin1214 <yanan.sun@okstate.edu>
2016-05-08 01:41:51 +0800

    Extend it for two layers
    Extend it for two layers

--- scripts_matlab/uniformly generate samples/test.m
+++ scripts_matlab/uniformly generate samples/test.m
@@ -1,12 +1,4 @@
 clc;clear;
-objs = 3; % set the number of objectives
-divs = 4; % how many divisions you want in each coordinate
-d = uniform_sample(objs, divs);
-% 
-% global points; % use to save all the sampled points
-% points = [];
-% p = zeros(1, objs); % temporary vector
-% 
-% uniform_generate(p, objs, divs, divs, 0);
-% % scatter3(points(:,1), points(:,2), points(:,3), 'LineWidth', 2);
-% % grid;
+p = uniform_sample(3,[2, 2]);
+
+scatter3(p(:,1), p(:,2), p(:,3));
