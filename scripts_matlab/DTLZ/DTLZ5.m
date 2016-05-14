89328e5653ba284df1c1f5144cf7c5c4caa76c57
sunkevin1214 <yanan.sun@okstate.edu>
2016-05-14 10:34:05 +0800

    Update the DTLZ5 and DTLZ6
    All the function are the same to that implemented in JMetal.

--- scripts_matlab/DTLZ/DTLZ5.m
+++ scripts_matlab/DTLZ/DTLZ5.m
@@ -3,11 +3,11 @@ function f = DTLZ5(  M, X )
     xm = X(M:end);
     g = sum((xm-0.5).^2);
     theta = (pi/(4*(1+g)))*(1+2*g*X);
-    theta(1) = X(1);
-    f(1) = (1+g)*prod(cos(theta(1:M-1).*pi.*0.5));
-    f(M) = (1+g)*sin(theta(1)*pi*0.5);
+    theta(1) = X(1).*pi.*0.5;
+    f(1) = (1+g)*prod(cos(theta(1:M-1)));
+    f(M) = (1+g)*sin(theta(1));
     for i =2:M-1
-        f(i) = (1+g)*prod(cos(theta(1:M-i).*pi./2))*sin(theta(M-i+1)*pi/2);
+        f(i) = (1+g)*prod(cos(theta(1:M-i)))*sin(theta(M-i+1));
     end
 
 end
--- scripts_matlab/DTLZ/DTLZ6.m
+++ scripts_matlab/DTLZ/DTLZ6.m
@@ -3,11 +3,11 @@ function f = DTLZ6(  M, X )
     xm = X(M:end);
     g = sum(xm.^(0.1));
     theta = (pi/(4*(1+g)))*(1+2*g*X);
-    theta(1) = X(1);
-    f(1) = (1+g)*prod(cos(theta(1:M-1).*pi./2));
-    f(M) = (1+g)*sin(theta(1)*pi/2);
+    theta(1) = X(1).*pi./2;
+    f(1) = (1+g)*prod(cos(theta(1:M-1)));
+    f(M) = (1+g)*sin(theta(1));
     for i =2:M-1
-        f(i) = (1+g)*prod(cos(theta(1:M-i).*pi./2))*sin(theta(M-i+1)*pi/2);
+        f(i) = (1+g)*prod(cos(theta(1:M-i)))*sin(theta(M-i+1));
     end
 
 end
